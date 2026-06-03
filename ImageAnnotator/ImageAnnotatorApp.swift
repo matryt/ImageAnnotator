195f30a9db8b73fff192cd4f585a820f1b33416d
matryt <mathieu.cuvelier@outlook.fr>
2026-06-03 19:07:37 -0400

    Initial Commit

---
+++ ImageAnnotator/ImageAnnotatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageAnnotatorApp.swift
+//  ImageAnnotator
+//
+//  Created by Mathieu CUVELIER on 03/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct ImageAnnotatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
